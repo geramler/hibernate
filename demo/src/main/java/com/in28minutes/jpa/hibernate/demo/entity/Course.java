4f51def9663aa1cd14676cc0e402c23de0672d0f
Gerardo Ramírez <geramler@gmail.com>
2021-01-25 18:56:08 -0600

    one to many relationship

--- demo/src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Course.java
+++ demo/src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Course.java
@@ -1,6 +1,8 @@
 package com.in28minutes.jpa.hibernate.demo.entity;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -16,7 +19,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 // @Table(name="CourseDetails")
 // @NamedQuery(name="query_get_all_courses", query="Select c from Course c")
 @NamedQueries(value = { @NamedQuery(name = "query_get_all_courses", query = "Select c from Course c"),
-						@NamedQuery(name = "query_get_100_steps_courses", query = "Select c From Course c where name like '%100 Steps'") })
+		@NamedQuery(name = "query_get_100_steps_courses", query = "Select c From Course c where name like '%100 Steps'") })
 public class Course {
 
 	@Id
@@ -31,6 +34,9 @@ public class Course {
 
 	@CreationTimestamp
 	private LocalDateTime createdDate;
+
+	@OneToMany(mappedBy="course")
+	private List<Review> reviews = new ArrayList<>();
 
 	// ensures that other classes which don't inherit from Course
 	// will not be able to use the constructor
@@ -52,6 +58,18 @@ public class Course {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void addReview(Review review) {
+		this.reviews.add(review);
+	}
+
+	public void removeReview(Review review) {
+		this.reviews.remove(review);
 	}
 
 	@Override
